b7842647a2fd1e35642cc270c04a2d74af2f105f
MasterAtWork <askmasteratwork@gmail.com>
2022-08-17 11:02:08 +0800

    replaced owner with role

--- contracts/EtherShield.sol
+++ contracts/EtherShield.sol
@@ -1,55 +1,48 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+pragma solidity 0.8.15;
 
-import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
-import '@openzeppelin/contracts/access/Ownable.sol';
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol";
 
-contract EtherShield is Ownable {
-
+contract EtherShield is AccessControl {
+    bytes32 public constant KEEPER_ROLE = keccak256("KEEPER_ROLE");
     mapping (address => bool) public keepers;
     mapping (address => address) public users; // origin address => destination address
 
-    event KeeperChanged(address indexed keeper, bool state);
-    event UserChanged(address indexed user, address destination);
+    constructor() {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(KEEPER_ROLE, msg.sender);
+    }
+
+    modifier onlyKeeper {
+        require(keepers[msg.sender], "OnlyKeeper:: Unauthorised");
+        _;
+    }
+
+    event KeeperChanged(address indexed keeper, bool state, address who, uint256 when);
+    event UserChanged(address indexed user, address destination, address who, uint256 when);
     event Rescue(address indexed user, address[] erc20Tokens);
 
-    function addKeepers(address _keeper) external onlyOwner {
-        keepers[_keeper] = true;
-        emit KeeperChanged(_keeper, true);
-    }
-
-    function removeKeeper(address _keeper) external onlyOwner {
-        keepers[_keeper] = false;
-        emit KeeperChanged(_keeper, false);
-    }
-
-    function addUser(address _destination) external {
-        users[msg.sender] = _destination;
-        emit UserChanged(msg.sender, _destination);
+    function addUser(address destination) external onlyRole(KEEPER_ROLE) {
+        require(destination != address(0), "AddUser:: Invalid address");
+        users[msg.sender] = destination;
+        emit UserChanged(msg.sender, destination, msg.sender, block.timestamp);
     }
 
     function disableUser() external {
         users[msg.sender] = address(0);
-        emit UserChanged(msg.sender, address(0));
+        emit UserChanged(msg.sender, address(0), msg.sender, block.timestamp);
     }
 
-    function rescue(address _user, address[] calldata _erc20Tokens) external onlyKeeper {
-        address destination = users[_user];
-        require(_user != address(0) ,'invalid _user address');
-        require(destination != address(0) ,'user is not registered');
-        uint256 tokenLength = _erc20Tokens.length;
-        for(uint i = 0 ;i < tokenLength; i++) {
-            ERC20 token = ERC20(_erc20Tokens[i]);
-            uint256 userBalance = token.balanceOf(_user);
+    function rescue(address _user, address[] calldata _erc20Tokens) external onlyRole(KEEPER_ROLE) {
+        require(_user != address(0) ,"Rescue:: Invalid _user address");
+        require(users[_user] != address(0) ,"Rescue:: User not registered");
+        for(uint i = 0 ;i < _erc20Tokens.length; i++) {
+            uint256 userBalance = IERC20(_erc20Tokens[i]).balanceOf(_user);
             if(userBalance > 0) {
-                token.transferFrom(_user, destination, userBalance);
+                TransferHelper.safeTransferFrom(_erc20Tokens[i], _user, users[_user], userBalance);
             }
         }
         emit Rescue(_user, _erc20Tokens);
-    }
-
-    modifier onlyKeeper {
-        require(keepers[msg.sender],'not a keeper');
-        _;
     }
 }
