650439e10409b34188d92db7cb222c45c21766e3
siddhanjay.godre <siddhanjay.godre@admins-MacBook-Pro.local>
2022-09-24 16:31:55 +0800

    modify contract

--- contracts/EtherShield.sol
+++ contracts/EtherShield.sol
@@ -11,33 +11,28 @@ contract EtherShield is AccessControl {
 
     constructor() {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
-        _grantRole(KEEPER_ROLE, msg.sender);
     }
 
-    modifier onlyKeeper {
-        require(keepers[msg.sender], "OnlyKeeper:: Unauthorised");
-        _;
-    }
-
-    event KeeperChanged(address indexed keeper, bool state, address who, uint256 when);
-    event UserChanged(address indexed user, address destination, address who, uint256 when);
+    event KeeperChanged(address indexed keeper, bool state);
+    event UserChanged(address indexed user, address destination);
     event Rescue(address indexed user, address[] erc20Tokens);
 
-    function addUser(address destination) external onlyRole(KEEPER_ROLE) {
+    function addUser(address destination) external {
         require(destination != address(0), "AddUser:: Invalid address");
         users[msg.sender] = destination;
-        emit UserChanged(msg.sender, destination, msg.sender, block.timestamp);
+        emit UserChanged(msg.sender, destination);
     }
 
     function disableUser() external {
         users[msg.sender] = address(0);
-        emit UserChanged(msg.sender, address(0), msg.sender, block.timestamp);
+        emit UserChanged(msg.sender, address(0));
     }
 
     function rescue(address _user, address[] calldata _erc20Tokens) external onlyRole(KEEPER_ROLE) {
         require(_user != address(0) ,"Rescue:: Invalid _user address");
         require(users[_user] != address(0) ,"Rescue:: User not registered");
-        for(uint i = 0 ;i < _erc20Tokens.length; i++) {
+        uint256 tokenLength = _erc20Tokens.length;
+        for(uint i = 0 ;i < tokenLength; i++) {
             uint256 userBalance = IERC20(_erc20Tokens[i]).balanceOf(_user);
             if(userBalance > 0) {
                 TransferHelper.safeTransferFrom(_erc20Tokens[i], _user, users[_user], userBalance);
@@ -45,4 +40,5 @@ contract EtherShield is AccessControl {
         }
         emit Rescue(_user, _erc20Tokens);
     }
+
 }
